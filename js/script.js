c52daf26c30c34c0a1d8f45740101f03aec0ab4a
DouglasSalvagni <douglassalvagni@gmail.com>
2019-04-19 18:41:06 +0100

    MenuMobile

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// OPEN MOBILE MENU
+
+document.querySelector('#openMobileMenu').onclick = function() {
+    document.getElementById('menu').style.transition = "0.5s";
+    document.getElementById('menu').style.left = "0";
+}
