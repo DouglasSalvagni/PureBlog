463827271e44f6b5b81a9df67206028b7f94f4eb
DouglasSalvagni <douglassalvagni@gmail.com>
2019-04-23 21:41:47 +0100

    search

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@
 document.querySelector('#openMobileMenu').onclick = function() {
     document.querySelector('nav').classList.toggle('mobileSideBarIsOpen')
 }
+
+//  OPEN SEARCH NAV HEADER
+
+document.querySelector('#OpenSearch').onclick = function() {
+    document.querySelector('#search').classList.toggle('searchWrapIsOpen')
+}
