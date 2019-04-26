d234b1373aa33dc0ea8e57048218e2ec3731f345
DouglasSalvagni <douglassalvagni@gmail.com>
2019-04-26 09:21:20 +0100

    close mobile menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-// OPEN MOBILE MENU
+// OPEN MOBILE MENU ---
 
-document.querySelector('#openMobileMenu').onclick = function() {
+document.querySelector('#openMobileMenu').onclick = ()=> {
     document.querySelector('.mobileSidebar').classList.toggle('mobileSidebar--open')
     document.querySelector('.contentOverlay').classList.toggle('contentOverlay--isVisible')
     document.querySelector('main').classList.toggle('open')
@@ -8,7 +8,31 @@ document.querySelector('#openMobileMenu').onclick = function() {
     document.querySelector('html').classList.toggle('hidden')
 }
 
-//  OPEN SEARCH NAV HEADER
+// CLOSE MOBILE MENU ---
+
+// clicking contentOverlay
+
+document.querySelector('.contentOverlay').onclick = () => {
+    document.querySelector('.mobileSidebar').classList.toggle('mobileSidebar--open')
+    document.querySelector('.contentOverlay').classList.toggle('contentOverlay--isVisible')
+    document.querySelector('main').classList.toggle('open')
+    document.querySelector('footer').classList.toggle('open')
+    document.querySelector('html').classList.toggle('hidden')
+}
+
+// changing screen resolution
+
+setInterval(()=>{
+    if (window.innerWidth >= 728) {
+        document.querySelector('.mobileSidebar').classList.remove('mobileSidebar--open')
+        document.querySelector('.contentOverlay').classList.remove('contentOverlay--isVisible')
+        document.querySelector('main').classList.remove('open')
+        document.querySelector('footer').classList.remove('open')
+        document.querySelector('html').classList.remove('hidden')
+    }
+}, 100)
+
+// OPEN SEARCH NAV HEADER
 
 document.querySelector('#OpenSearch').onclick = function() {
     document.querySelector('#search').classList.toggle('searchWrapIsOpen')
