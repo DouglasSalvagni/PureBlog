72879a24e0d36dbd3c1421b56b21960c7d1c8eaa
unknown <douglassalvagni@gmail.com>
2019-05-06 18:22:17 +0100

    correções

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@
 document.querySelector('#openMobileMenu').onclick = ()=> {
     document.querySelector('.mobileSidebar').classList.toggle('mobileSidebar--open')
     document.querySelector('.contentOverlay').classList.toggle('contentOverlay--isVisible')
+    document.querySelector('.searchWrap').classList.toggle('searchWrap--open')
     document.querySelector('main').classList.toggle('open')
     document.querySelector('footer').classList.toggle('open')
-    document.querySelector('html').classList.toggle('hidden')
+    document.querySelector('body').classList.toggle('hidden')
 }
 
 // CLOSE MOBILE MENU ---
@@ -15,6 +16,7 @@ document.querySelector('#openMobileMenu').onclick = ()=> {
 document.querySelector('.contentOverlay').onclick = () => {
     document.querySelector('.mobileSidebar').classList.toggle('mobileSidebar--open')
     document.querySelector('.contentOverlay').classList.toggle('contentOverlay--isVisible')
+    document.querySelector('.searchWrap').classList.toggle('searchWrap--open')
     document.querySelector('main').classList.toggle('open')
     document.querySelector('footer').classList.toggle('open')
     document.querySelector('html').classList.toggle('hidden')
@@ -26,6 +28,7 @@ setInterval(()=>{
     if (window.innerWidth >= 728) {
         document.querySelector('.mobileSidebar').classList.remove('mobileSidebar--open')
         document.querySelector('.contentOverlay').classList.remove('contentOverlay--isVisible')
+        document.querySelector('.searchWrap').classList.toggle('searchWrap--open')
         document.querySelector('main').classList.remove('open')
         document.querySelector('footer').classList.remove('open')
         document.querySelector('html').classList.remove('hidden')
@@ -35,20 +38,21 @@ setInterval(()=>{
 //MOBILE MENU ---
 
 var accordion = document.querySelectorAll('.accordion')
-console.log(accordion)
 
 for (let i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener('click', function() {
         this.classList.toggle('rotate')
         let panel = this.parentElement.nextElementSibling
         panel.classList.toggle('panel-is-open')
-        console.log(this.parentElement.nextElementSibling)
     })
 }
 
 
 // OPEN SEARCH NAV HEADER
 
+document.querySelector('#openSearch').onclick = function() {
+    document.querySelector('#search').classList.toggle('searchNavWrapIsOpen')
+}
 
 
 
