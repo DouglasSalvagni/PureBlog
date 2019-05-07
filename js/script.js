108cb041ed98fcd1b38f28fb569624e6ee533995
DouglasSalvagni <douglassalvagni@gmail.com>
2019-05-07 10:37:25 +0100

    bug shadow fixed and ajustments

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ setInterval(()=>{
     if (window.innerWidth >= 728) {
         document.querySelector('.mobileSidebar').classList.remove('mobileSidebar--open')
         document.querySelector('.contentOverlay').classList.remove('contentOverlay--isVisible')
-        document.querySelector('.searchWrap').classList.toggle('searchWrap--open')
+        document.querySelector('.searchWrap').classList.remove('searchWrap--open')
         document.querySelector('main').classList.remove('open')
         document.querySelector('footer').classList.remove('open')
         document.querySelector('html').classList.remove('hidden')
